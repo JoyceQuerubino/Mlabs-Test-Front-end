e09785cea9be6dc2166997aa232acc812db180bc
Joyce Querubino <joycequerubino5@gmail.com>
2020-10-19 16:36:46 -0300

    Finalizando estilização e ação do botão de res sociais

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,10 +1,19 @@
-console.log("oiii");
-
 //Adicionando click no botão de media social
 var btnSocialMedia = document.getElementsByClassName("btn-social-media");
 
 for (var i = 0; i < btnSocialMedia.length; i++) {
   btnSocialMedia[i].onclick = function () {
+    //removendo
+    var removeClass = btnSocialMedia[0];
+    while (removeClass) {
+      if (removeClass.tagName === "BUTTON") {
+        //remove
+        removeClass.classList.remove("click-social-media");
+      }
+      //passando para o próximo
+      removeClass = removeClass.nextSibling;
+    }
+
     this.classList.add("click-social-media");
   };
 }
