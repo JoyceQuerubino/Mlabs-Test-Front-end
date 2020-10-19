647dfa7cc95c53237884563e6aed9a3c22f2abec
Joyce Querubino <joycequerubino5@gmail.com>
2020-10-19 13:08:43 -0300

    Alterando dados para consumo api e estilo dos botões de redes sociais

---
+++ db/social-networks.js
@@ -0,0 +1,39 @@
+// exportando arquivos para o servidor
+module.exports = [
+  {
+    id: 1,
+    name: "Instagram",
+    class: "fab fa-instagram",
+    status: "enabled",
+  },
+  {
+    id: 2,
+    name: "Linkedin",
+    class: "fab fa-linkedin-in",
+    status: "enabled",
+  },
+  {
+    id: 3,
+    name: "Youtube",
+    class: "fab fa-youtube",
+    status: "disabled",
+  },
+  {
+    id: 4,
+    name: "Pinterest",
+    class: "fab fa-pinterest",
+    status: "disabled",
+  },
+  {
+    id: 5,
+    name: "Twitter",
+    class: "fab fa-twitter",
+    status: "disabled",
+  },
+  {
+    id: 6,
+    name: "Facebook",
+    class: "fab fa-facebook-f",
+    status: "disabled",
+  },
+];
