970d8ea428a522a3db6abc2188de1f70313b3ebf
Joyce Querubino <joycequerubino5@gmail.com>
2020-10-21 22:35:41 -0300

    Atualizando responsividade e add ações do botão de visualização do preview

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,9 +1,9 @@
 // Ações dos botões de redes sociais
-var btnInstagram = document.getElementById("1");
-var btnLinkedin = document.getElementById("2");
-var postInstagram = document.getElementById("post-instagram");
-var postLinkedin = document.getElementById("post-linkedin");
-var esperando = document.getElementById("waiting-post");
+const btnInstagram = document.getElementById("1");
+const btnLinkedin = document.getElementById("2");
+const postInstagram = document.getElementById("post-instagram");
+const postLinkedin = document.getElementById("post-linkedin");
+const esperando = document.getElementById("waiting-post");
 
 btnInstagram.onclick = function () {
   this.classList.toggle("click-social-media");
@@ -17,4 +17,22 @@ btnLinkedin.onclick = function () {
   btnInstagram.classList.toggle("btn-disabled");
   postLinkedin.classList.toggle("post-on");
   esperando.classList.toggle("off");
+};
+
+// Ação do botão de visualizar preview responsivo
+
+const modalOverlay = document.querySelector(".modal-overlay");
+const btnModal = document.querySelector(".btn-preview-post");
+const btnCloseModal = document.querySelector(".close-icon");
+
+btnModal.onclick = function () {
+  if (btnInstagram.classList.contains("click-social-media")) {
+    modalOverlay.classList.add("active");
+  } else if (btnLinkedin.classList.contains("click-social-media")) {
+    modalOverlay.classList.add("active");
+  }
+};
+
+btnCloseModal.onclick = function () {
+  modalOverlay.classList.remove("active");
 };
